46aa8150211e7682004059de1634980b08665729
Josafath17 <105898971+Josafath17@users.noreply.github.com>
2024-04-15 01:02:21 -0600

    video update
    se actualizó el controlador y el modelo de videos

--- models/videoModel.js
+++ models/videoModel.js
@@ -1,15 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const video = new Schema({
   name: { type: String },
   url: { type: String },
+  description: { type: String },
   playlist: {
     type: mongoose.ObjectId,
-    ref: 'Playlist'
+    ref: "Playlist",
   },
-
-
 });
 
-module.exports = mongoose.model('Video', video);
+module.exports = mongoose.model("Video", video);
